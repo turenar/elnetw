3c08232f2ad439ee31339f71bd108d9c144d83e6
Turenar <konoe@turenar.mydns.jp>
2014-11-08 21:18:27 +0900

    add NotifyHandler (issue #68)

--- tools/executor.sh
+++ tools/executor.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 cpcachef=elnetw-core/target/.classpath.cache
+if [[ $OSTYPE == "cygwin" || $OSTYPE == "msys" ]]; then
+	PATHSEP=";"
+else
+	PATHSEP=":"
+fi
 
 test -d elnetw-core/target/bin || mvn install
 test -f ${cpcachef} || ( \
 	cd elnetw-core; \
 	mvn dependency:build-classpath -Dmdep.outputFile=../${cpcachef}; )
 
-java -Delnetw.home=tools -cp elnetw-core/target/test:elnetw-core/target/bin:$(cat ${cpcachef}) $@
+java -Delnetw.home=tools -cp elnetw-core/target/test${PATHSEP}elnetw-core/target/bin${PATHSEP}$(cat ${cpcachef}) $@
